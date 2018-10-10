9d655082a98e7ddde84f4a9cd5186d664baac75f
Joris Wiethase <joriswiethase@Joriss-iMac.lan>
2018-10-10 09:42:31 +0100

    prepared effort data sheet

--- app.R
+++ app.R
@@ -3,6 +3,16 @@
 rm(list = ls(all=TRUE))  
 library(dplyr)
 library(ggplot2)
+library(lubridate)
 
 data <- read.csv('OpWall_banding_data.csv')
 effort <- read.csv('OpWall band data effort.csv')
+
+effort <- effort %>% mutate(open = sprintf("%04d",open), close = sprintf("%04d",close)) %>% 
+  mutate(open= sub("([[:digit:]]{2,2})$", ":\\1", open), close= sub("([[:digit:]]{2,2})$", ":\\1", close) ) %>% 
+  mutate(open= dmy_hm(paste(Date, open, sep=" ")), close= dmy_hm(paste(Date, close, sep=" ")),
+         hours= close-open, 
+         net_hours_day= hours*Nets) %>% 
+  group_by(Date) %>% 
+  mutate(total_net_hours_day=sum(net_hours_day)) %>% ungroup()
+
