58483d8ab02840c681bd8f5255a9788faa75c7a7
Joris Wiethase <joriswiethase@Joriss-iMac.lan>
2018-10-11 12:38:03 +0100

    moved back to version without interactive column selection. Finished error seeking inputs

--- app.R
+++ app.R
@@ -1,4 +1,4 @@
-# Author: Joris Wiethase
+# 
 # 
 rm(list = ls(all=TRUE))  
 library(shiny)
@@ -6,32 +6,40 @@ library(DT)
 library(stringdist)
 library(reshape2)
 library(magrittr)
+library(lubridate)
+library(dplyr)
+library(stringr)
 
 # Make the user interface
 ui <- shiny::fluidPage(theme = "bootstrap.css",
-                           # Add a side panel for inputs
-                           shiny::sidebarPanel(width = 2,
-                                               shiny::fileInput(inputId = 'dataset', 
-                                                                   label = h4('Choose .csv file to upload'),
-                                                                   accept = c('.csv')
-                                                  ),
-                                                  helpText("Warning: Data set has to include all of the following columns:",
-                                                           "band_size  band_sequence  species  Date(as ymd)  year"),
-                                               shiny::selectInput(inputId = "errorSeek", 
-                                                                  label = h4("Choose error seeking option"),
-                                                                  choices = c('None',
-                                                                              'NA in species or band number',
-                                                                              'Species - band number discrepancies',
-                                                                              'Same-season recaptures', 
-                                                                              'Band sequence discrepancies',
-                                                                              'Unusual band size')),
-                                               hr(),
-                                               selectInput("colID", "Select column for typo check", ' '),
-                                               actionButton("go", "Check!", icon = icon("angle-double-right"), width = "auto")
-                                               
-                                               ),
+                       # Add a side panel for inputs
+                       shiny::sidebarPanel(width = 2,
+                                           shiny::fileInput(inputId = 'dataset', 
+                                                            label = h4('Choose .csv file to upload'),
+                                                            accept = c('.csv')
+                                           ),
+                                           helpText("Warning: Dataset has to include all of the following column names:"),
+                                           hr(),
+                                           helpText("'Band.ID' (Format: 'Size-Sequence')"),
+                                           helpText("'Species'"),
+                                           helpText("'Date' (Format: dmy)"),
+                                           helpText("'Recap (Format: Y/N'"),
+                                           hr(),
+                                           shiny::selectInput(inputId = "errorSeek", 
+                                                              label = h4("Choose error seeking option"),
+                                                              choices = c('None',
+                                                                          'NA in species or band number',
+                                                                          'Species - band number discrepancies',
+                                                                          'Same-season recaptures', 
+                                                                          'Band sequence discrepancies',
+                                                                          'Unusual band size')),
+                                           hr(),
+                                           selectInput("colID", "Select column for typo check", ' '),
+                                           actionButton("go", "Check!", icon = icon("angle-double-right"), width = "auto")
+                                           
+                       ),
                        mainPanel(DTOutput("table"), style = "height:900px; overflow-y: scroll;overflow-x: scroll;")
-                       )
+)
 
 # Make the server functions
 server <- function(input, output, session) {
@@ -39,8 +47,12 @@ server <- function(input, output, session) {
     req(input$dataset)
     data <- read.csv(input$dataset$datapath) 
     if("X" %in% names(data) == TRUE){
-      data <- data %>% select(-X)
+      data <- data %>% dplyr::select(-X)
     }
+  })
+  
+  observeEvent(data(), {
+    updateSelectInput(session, "colID", choices=colnames(data()))
   })
   
   shiny::observeEvent({
@@ -50,8 +62,8 @@ server <- function(input, output, session) {
       observe({
         if(input$errorSeek == 'None'){
           output$table <-  renderDT({
-          data()
-            }, options = list(scrollX = TRUE, paging = TRUE), editable = TRUE)
+            data()
+          }, options = list(scrollX = TRUE, paging = TRUE), editable = TRUE)
         }
       })
       
@@ -59,7 +71,13 @@ server <- function(input, output, session) {
         if(input$errorSeek == 'NA in species or band number'){
           output$table <-  renderDT({
             data <- data()
-            data <- data %>% filter(is.na(Band.ID), is.na(species))
+            names <- colnames(data)
+            name.spec <- names[grepl("Species", names, ignore.case=TRUE) == TRUE]
+            names(data)[names(data) == name.spec] <- 'Species'
+            data$band_size <- sapply(strsplit(as.character(data$Band.ID), split="-"), `[`, 1)
+            data$band_sequence <- sapply(strsplit(as.character(data$Band.ID), split="-"), `[`, 2)
+            
+            data <- data %>% filter(is.na(band_sequence), is.na(Species))
           }, options = list(scrollX = TRUE, paging = FALSE), editable = TRUE)
         }
       })
@@ -68,9 +86,16 @@ server <- function(input, output, session) {
         if(input$errorSeek == 'Species - band number discrepancies'){
           output$table <-  renderDT({
             data <- data()
-            data <- data %>% group_by(Band.ID) %>% dplyr::filter(length(unique(species)) > 1) %>% 
+            names <- colnames(data)
+            name.spec <- names[grepl("Species", names, ignore.case=TRUE) == TRUE]
+            names(data)[names(data) == name.spec] <- 'Species'
+            data$band_size <- sapply(strsplit(as.character(data$Band.ID), split="-"), `[`, 1)
+            data$band_sequence <- sapply(strsplit(as.character(data$Band.ID), split="-"), `[`, 2)
+            
+            
+            data <- data %>% group_by(Band.ID) %>% dplyr::filter(length(unique(Species)) > 1) %>% 
               arrange(Band.ID) %>%
-              select(Band.ID, species, everything())
+              dplyr::select(Band.ID, Species, everything())
           }, options = list(scrollX = TRUE, paging = FALSE), editable = TRUE)
         }
       })
@@ -79,13 +104,20 @@ server <- function(input, output, session) {
         if(input$errorSeek == 'Same-season recaptures'){
           output$table <-  renderDT({
             data <- data()
+            names <- colnames(data)
+            name.spec <- names[grepl("Species", names, ignore.case=TRUE) == TRUE]
+            names(data)[names(data) == name.spec] <- 'Species'
+            
+            data$band_size <- sapply(strsplit(as.character(data$Band.ID), split="-"), `[`, 1)
+            data$band_sequence <- sapply(strsplit(as.character(data$Band.ID), split="-"), `[`, 2)
+            
             data <- data %>% 
-              mutate(Date = ymd(Date), field.season=as.factor(year)) %>% 
+              mutate(Date = dmy(Date)) %>% 
               group_by(Band.ID) %>% arrange(Date) %>% mutate(days_diff = difftime(Date, lag(Date), 
-                                                                                  units='days')) %>% 
+                                                                                 units='days')) %>% 
               filter(days_diff < 300) %>% 
               arrange(days_diff) %>%
-              select(days_diff, Date, species, Band.ID, everything())
+              dplyr::select(days_diff, Date, Species, Band.ID, everything())
           }, options = list(scrollX = TRUE, paging = FALSE), editable = TRUE)
         }
       })
@@ -94,35 +126,51 @@ server <- function(input, output, session) {
         if(input$errorSeek == 'Band sequence discrepancies'){
           output$table <-  renderDT({
             data <- data()
-            data <- data %>% group_by(band_size) %>% arrange(band_size, band_sequence) %>% mutate(band_diff = band_sequence - lag(band_sequence)) %>% 
-              filter(band_diff > 1) %>% 
-              select(band_size, band_sequence,  band_diff, year, species, everything())
-          }, options = list(scrollX = TRUE, paging = TRUE), editable = TRUE)
-        }
-      })
-
-      observe({
-        if(input$errorSeek == 'Unusual band size'){
-          output$table <-  renderDT({
-            data <- data()
-            data <- data %>% group_by(species, band_size) %>% mutate(N=length(band_size)) %>% 
-              group_by(species) %>% 
-              mutate(perc = round((100*N)/sum(unique(N)), digits = 1)) %>% 
-              filter(perc <= 10) %>% 
-              select(band_size, species, perc, everything()) %>% 
-              arrange(species, perc, band_size)
+            names <- colnames(data)
+            name.Recap <- names[grepl("Recap", names, ignore.case=TRUE) == TRUE]
+            names(data)[names(data) == name.Recap] <- 'Recap'
+            name.spec <- names[grepl("Species", names, ignore.case=TRUE) == TRUE]
+            names(data)[names(data) == name.spec] <- 'Species'
+            
+            
+            data$band_size <- sapply(strsplit(as.character(data$Band.ID), split="-"), `[`, 1)
+            data$band_sequence <- as.numeric(sapply(strsplit(as.character(data$Band.ID), split="-"), `[`, 2))
+          
+            data <- data %>% filter(Recap == 'N') %>% group_by(band_size) %>% arrange(band_size, band_sequence) %>% mutate(band_diff = band_sequence - lag(band_sequence))
+            data$band_diff[is.na(data$band_diff)] <- 0
+            data <- data %>% 
+              filter(band_diff > 2) %>% 
+              dplyr::select(band_size, band_sequence,  band_diff, Species, everything()) %>% 
+              arrange(band_size, band_sequence,  band_diff)
           }, options = list(scrollX = TRUE, paging = FALSE), editable = TRUE)
         }
       })
       
-      observeEvent(data(), {
-        updateSelectInput(session, "colID", choices=colnames(data()))
+      observe({
+        if(input$errorSeek == 'Unusual band size'){
+          output$table <-  renderDT({
+            data <- data()
+            names <- colnames(data)
+            name.spec <- names[grepl("Species", names, ignore.case=TRUE) == TRUE]
+            names(data)[names(data) == name.spec] <- 'Species'
+            data$band_size <- sapply(strsplit(as.character(data$Band.ID), split="-"), `[`, 1)
+            data$band_sequence <- sapply(strsplit(as.character(data$Band.ID), split="-"), `[`, 2)
+            
+            data <- data %>% group_by(Species, band_size) %>% mutate(N=length(band_size)) %>% 
+              group_by(Species) %>% 
+              mutate(perc = round((100*N)/sum(unique(N)), digits = 1)) %>% 
+              filter(perc <= 10) %>% 
+              dplyr::select(band_size, Species, perc, everything()) %>% 
+              arrange(Species, perc, band_size)
+          }, options = list(scrollX = TRUE, paging = FALSE), editable = TRUE)
+        }
       })
       
+      
       observeEvent(input$go, {
-          output$table <-  renderDT({
-            input$go
-            isolate({
+        output$table <-  renderDT({
+          input$go
+          isolate({
             data <- data()
             colID <- input$colID
             names(data)[names(data) == colID] <- 'colID'
@@ -132,14 +180,14 @@ server <- function(input, output, session) {
             
             out <- melt(m) %>% distinct() %>% filter(value > 0 & value < 2)
             data <- merge(data, out, by.x = 'colID', by.y = 'Var1', all.x=TRUE) %>% filter(!is.na(value)) %>% 
-              select(colID, Var2, value, everything()) %>% rename(StringDiff = value)
+              dplyr::select(colID, Var2, value, everything()) %>% rename(StringDiff = value)
             
-            })
-            }, options = list(scrollX = TRUE, paging = FALSE), editable = TRUE)
+          })
+        }, options = list(scrollX = TRUE, paging = FALSE), editable = TRUE)
       })
       
       
-      })
-  }
+    })
+}
 
 shiny::shinyApp(ui, server)
