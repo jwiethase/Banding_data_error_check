fd31c25d9f34e0ec9ed04d64d86cca3a41c68c85
Joris Wiethase <joriswiethase@Joriss-iMac.lan>
2018-10-13 13:45:51 +0100

    Make code more concise
    Removed redundant code, by merginh all 'observe' blocks into one

--- app.R
+++ app.R
@@ -33,7 +33,7 @@ ui <- shiny::fluidPage(theme = "bootstrap.css",
                                                                           'Same-season recaptures', 
                                                                           'Band sequence discrepancies',
                                                                           'Unusual band size')),
-                                           # uiOutput("slider"),
+                                           uiOutput("slider"),
                                            hr(),
                                            selectInput("colID", "Select column for typo check", ' '),
                                            actionButton("go", "Check!", icon = icon("angle-double-right"), width = "auto")
@@ -64,27 +64,22 @@ server <- function(input, output, session) {
     updateSelectInput(session, "colID", choices=colnames(data()))
   })
   
+
+  
   shiny::observeEvent({
     input$dataset
     input$errorSeek}, {
-      
       observe({
         if(input$errorSeek == 'None'){
           output$table <-  renderDT({
             data()
           }, options = list(scrollX = TRUE, paging = TRUE), editable = TRUE)
         }
-      })
-      
-      observe({
         if(input$errorSeek == 'NA in species or band number'){
           output$table <-  renderDT({
             data() %>% filter(is.na(band_sequence), is.na(Species))
           }, options = list(scrollX = TRUE, paging = FALSE), editable = TRUE)
         }
-      })
-      
-      observe({
         if(input$errorSeek == 'Species - band number discrepancies'){
           output$table <-  renderDT({
             data() %>% 
@@ -93,48 +88,43 @@ server <- function(input, output, session) {
               dplyr::select(Band.ID, Species, everything())
           }, options = list(scrollX = TRUE, paging = FALSE), editable = TRUE)
         }
-      })
-      
-      observe({
         if(input$errorSeek == 'Same-season recaptures'){
           output$table <-  renderDT({
             data() %>% 
               mutate(Date = dmy(Date)) %>% 
               group_by(Band.ID) %>% arrange(Date) %>% mutate(days_diff = difftime(Date, lag(Date), 
-                                                                                 units='days')) %>% 
+                                                                                  units='days')) %>% 
               filter(days_diff < 300) %>% 
               arrange(days_diff) %>%
               dplyr::select(days_diff, Date, Species, Band.ID, everything())
             
           }, options = list(scrollX = TRUE, paging = FALSE), editable = TRUE)
         }
-      })
-      
-      observe({
         if(input$errorSeek == 'Band sequence discrepancies'){
-          output$table <-  renderDT({
+          seq_data <- reactive({
             data <- data() %>% filter(Recap == 'N') %>% group_by(band_size) %>% arrange(band_size, band_sequence) %>% mutate(band_diff = band_sequence - lag(band_sequence)) 
             data$band_diff[is.na(data$band_diff)] <- 0
-            
-            # observe({
-            # output$slider <- renderUI({
-            #   sliderInput("inSlider", "Band difference range", min=min(data$band_diff), max=max(data$band_diff), value=2)
-            # })
-            # })
-            
-            data <- data %>% 
-              filter(band_diff > 2) %>% 
-              dplyr::select(band_size, band_sequence,  band_diff, Species, everything()) %>% 
-              arrange(band_size, band_sequence,  band_diff)
             data
-          }, options = list(scrollX = TRUE, paging = FALSE), editable = TRUE)
+          })
+          output$slider <- renderUI({
+            seq_data <- seq_data()
+            sliderInput("inSlider", "Band difference range", min=min(seq_data$band_diff), max=max(seq_data$band_diff), value=2)
+          })
+          observeEvent(input$inSlider, {
+            output$table <-  renderDT({
+              seq_data() %>% 
+                filter(band_diff > input$Slider) %>% 
+                dplyr::select(band_size, band_sequence,  band_diff, Species, everything()) %>% 
+                arrange(band_size, band_sequence,  band_diff)
+              data
+            }, options = list(scrollX = TRUE, paging = FALSE), editable = TRUE)
+            
+          })
+
         }
-      })
-      
-      observe({
         if(input$errorSeek == 'Unusual band size'){
           output$table <-  renderDT({
-           data() %>% 
+            data() %>% 
               group_by(Species, band_size) %>% mutate(N=length(band_size)) %>% 
               group_by(Species) %>% 
               mutate(perc = round((100*N)/sum(unique(N)), digits = 1)) %>% 
@@ -144,7 +134,7 @@ server <- function(input, output, session) {
           }, options = list(scrollX = TRUE, paging = FALSE), editable = TRUE)
         }
       })
-      
+
       observeEvent(input$go, {
         output$table <-  renderDT({
           input$go
